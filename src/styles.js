c3edb50ce1bdf21001d0429b4956c5020a4a3d4a
Michal Szorád <miso.szorad@gmail.com>
2017-08-31 13:53:50 +0200

    Changed testing styles

--- src/styles.js
+++ src/styles.js
@@ -1,14 +1,17 @@
 export default {
     title: {
         boxShadow: '0 0 20px black',
+        margin: 5,
         background: 'rgba(256,256,256,0.5)'
     },
 
     button: {
+        opacity: 0.5,
         boxShadow: '0 0 20px black',
         background: 'rgba(256,256,256,0.5)'
     },
     input: {
+        borderRadius: 5,
         padding: 5,
         border: '1px solid #efefef',
         outline: 'none'
